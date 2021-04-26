4ff2c00d95b3668d1495eb6bca8faf3f44410c46
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-26 17:38:42 -0400

    commit tree 1b020336ea1ed12dfe15a06403875f8834e296ae

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
